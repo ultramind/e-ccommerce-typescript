32372e7a230cdb41d326c81c0452c84a0666f40b
Akachukwu <udechukwuakachukwu@gmail.com>
2024-12-24 00:27:19 +0100

    Added image to the footer top

--- src/ui/FooterTop.tsx
+++ src/ui/FooterTop.tsx
@@ -5,21 +5,21 @@ const FooterTop = () => {
     {
       name: "Free shipping",
       imageSrc:
-        "https://tailwindui.com/img/ecommerce/icons/icon-shipping-simple.svg",
+        "/shipping.svg",
       description:
         "It's not actually free we just price it into the products. Someone's paying for it, and it's not us.",
     },
     {
       name: "10-year warranty",
       imageSrc:
-        "https://tailwindui.com/img/ecommerce/icons/icon-warranty-simple.svg",
+        "/warranty.png",
       description:
         "If it breaks in the first 10 years we'll replace it. After that you're on your own though.",
     },
     {
       name: "Exchanges",
       imageSrc:
-        "https://tailwindui.com/img/ecommerce/icons/icon-exchange-simple.svg",
+        "/exchange.svg",
       description:
         "If you don't like it, trade it to one of your friends for something of theirs. Don't send it here though.",
     },
